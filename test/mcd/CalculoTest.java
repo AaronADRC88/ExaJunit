c57de96f113d173eaa0e4ee0135426d2cc9d5e44
aferreiradominguez <aferreiradominguez@atanasoft05.danielcastelao.org>
2015-06-02 10:36:08 +0200

    Tests pila y método hechos

---
+++ test/mcd/CalculoTest.java
@@ -0,0 +1,40 @@
+
+
+package mcd;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class CalculoTest {
+    
+    public CalculoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+ 
+    /**
+     * Test of obtenerMCD method, of class Calculo.
+     */
+    @Test
+    public void testObtenerMCD() {
+        System.out.println("obtenerMCD");
+        int num1 = 10;
+        int num2 = 4;
+        Calculo instance = new Calculo();
+        int expResult = 2;
+        int result = instance.obtenerMCD(num1, num2);
+        assertEquals(expResult, result,0);
+        
+    }
+    
+}
